985e68c07973cfbd29c78dd8b0b7720313246394
tom creighton <sto_rage@yahoo.com>
2014-07-10 09:13:42 -0400

    added potty app and reg templates

--- canipee/urls.py
+++ canipee/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    # existing patterns here...
+    (r'^accounts/login/$',  login),
+    (r'^accounts/logout/$', logout),
+
 )
